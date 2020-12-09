2b0f519fd64ad88ad83804d8b435248833cd83b0
Alex Harker <aharker@morpheusdata.com>
2020-12-09 15:39:55 -0500

    small code change

--- accDisc.py
+++ accDisc.py
@@ -6,17 +6,17 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 role = 'OrganizationAccountAccessRole'
 creds = []
 morphurl = 'https://68.39.65.192'
-apitoken = sys.argv[1]
+apitoken = '4ed299f0-5a73-4906-9ed1-758931b678b1' #sys.argv[1]
 
 #Instantiate 'organization' object
 org_client = boto3.client('organizations',
-	aws_access_key_id=sys.argv[2],
-    aws_secret_access_key=sys.argv[3])
+	aws_access_key_id='AKIAQHCX3NJFB2OG5YX6', #sys.argv[2],
+    aws_secret_access_key='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') #sys.argv[3])
 
 #instantiate 'sts' object
 sts_client = boto3.client('sts',
-	aws_access_key_id=sys.argv[2],
-    aws_secret_access_key=sys.argv[3])
+	aws_access_key_id='AKIAQHCX3NJFB2OG5YX6', #sys.argv[2],
+    aws_secret_access_key='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') #sys.argv[3])
 
 #Call list_accounts() method on organization object
 org_response = org_client.list_accounts()
@@ -28,49 +28,52 @@ for i in range(0, int(len(org_response['Accounts']))):
 	#Instantiate account name
 	name = org_response['Accounts'][i]['Name']
 	
-	if (added.date() - datetime.today().date()).days == -1: #If the account was added to the organization on the prior date (yesterday)
+	if (added.date() - datetime.today().date()).days == 0: #If the account was added to the organization on the prior date (yesterday)
 		#Instantiate the ID for the AWS account currently being iterated on
 		current = org_response['Accounts'][i]['Id']
 	else:
 		#Skip to the next iteration if the currently considered account was not added to the organization in the last day
 		continue
 
-	try:
-		#Run assume_role() method on sts object
-		sts_response = sts_client.assume_role(
-			RoleArn=f'arn:aws:iam::{current}:role/{role}',
-			RoleSessionName='mySession')
+	#Run assume_role() method on sts object
+	sts_response = sts_client.assume_role(
+		RoleArn=f'arn:aws:iam::{current}:role/{role}',
+		RoleSessionName='mySession')
 
-		#Append the temporary creds/role arn as a tuple to a list called 'creds'
-		creds.append((name, 
-			#sts_response['Credentials']['AccessKeyId'],  
-			#sts_response['Credentials']['SecretAccessKey'], 
-			#sts_response['Credentials']['SessionToken'], 
-			sts_response['AssumedRoleUser']['Arn']))
+	#Append the temporary creds/role arn as a tuple to a list called 'creds'
+	creds.append((name, 
+		#sts_response['Credentials']['AccessKeyId'],  
+		#sts_response['Credentials']['SecretAccessKey'], 
+		#sts_response['Credentials']['SessionToken'], 
+		sts_response['AssumedRoleUser']['Arn']))
 
-		try:
-			#Attempt to integrate a new cloud with Morpheus using the temporary creds from assumerole using Morpheus API
-			requestHeaders = {'Content-type': 'application/json', 
-				'Authorization': f'Bearer {apitoken}'}
+	#Attempt to integrate a new cloud with Morpheus using the temporary creds from assumerole using Morpheus API
+	requestHeaders = {'Content-type': 'application/json', 
+		'Authorization': f'Bearer {apitoken}'}
 
-			requests.post(f'{morphurl}/api/zones', 
-				data={"zone": {"name": name, "description": "None", "groupId": 1, "zoneType": {"code": "amazon"},
-					"config": {"certificateProvider": "internal", "endpoint": "ec2.us-east-1.amazonaws.com", "accessKey": sys.argv[2], "secretKey": sys.argv[3], "vpc": "None", "importExisting": "off"},
-					"code": "None",
-					"location": "None",
-					"visibility": "private"}
-					}, 
-				headers=requestHeaders, 
-				verify=False)
+	requests.post(f'{morphurl}/api/zones', 
+		json={
+			"zone": {
+				"name": name, 
+				"description": None, 
+				"groupId": 1, 
+				"zoneType": {
+					"code": "amazon"
+				},
+				"config": {
+					"certificateProvider": "internal", 
+					"endpoint": "ec2.us-east-1.amazonaws.com", 
+					"accessKey": 'AKIAQHCX3NJFB2OG5YX6', 
+					"secretKey": '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', 
+					"vpc": "All", 
+					"importExisting": "off"
+				},
+				"code": None,
+				"location": None,
+				"visibility": "private"
+				}
+			},
+		headers=requestHeaders, 
+		verify=False)
 
-			print(f'Account {current}: Added')
-		
-		except:
-			#Print friendly message if the API post request fails for any reason
-			print('API post failed')
-			continue
-
-	except:
-		#Print friendly message if there is a failure running the assume_role() method or if there's an issue loading the 'creds' list
-		print(f'Account {current}: Failed to add')
-
+	print(f'Account {current}: Added')
